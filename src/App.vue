c64c940fa190e1947045a5644401c020458a6279
James Walsh <jamesandersonwalsh@gmail.com>
2022-11-17 16:07:44 -0700

    Basics of event handling using v directives

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,61 @@
 -->
 <!-- * Logic Layer: -->
 <script setup>
-import { ref } from 'vue';
-const useChangeWithDelay = (state, newVal, delay) => {
-  setTimeout(() => {
-    state.value = newVal
-  }, delay)
-}
-const product = ref('Socks') // Vue equivalent of useState in React.
+import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
-useChangeWithDelay(product, 'New Socks', 1000)
+const product = ref('Socks')
+const image = ref(socksGreenImage)
+
+const inStock = ref(true)
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage }
+])
+
+const addToCart = () => {
+  cart.value +=1
+}
+const updateImage = (variantImage) => {
+  image.value = variantImage
+}
+
+const cart = ref(0)
+
 </script>
 
-<!-- * View Layer: -->
+<!--
+  * View Layer:
+    Template bindings support partial binding, any javascript expression can go in here.
+-->
 <template>
-  <!-- Supports partial binding, any javascript expression can go in here. -->
-  <h1>{{ product }}</h1>
+  <div class="nav-bar" />
+  <div class="cart">Cart({{cart}})</div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image" alt="green socks">
+      </div>
+      <div class="product-info">
+        <h1>{{product}}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{detail}}</li>
+        </ul>
+        <div
+          v-for="variant in variants"
+          :key="variant.id"
+          @mouseover="updateImage(variant.image)"
+          >
+          {{variant.color}}
+        </div>
+        <button class="button" v-on:click="addToCart">Add to Cart</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <!-- * Style Layer: -->
